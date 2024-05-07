9f8fea8c81eafb7689d06418ff908623592e453e
Zazzaro <sassaro01cristian@gmail.com>
2024-05-07 16:09:58 +0200

    changed import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:expenses_tracker_flutter/expenses.dart';
+import 'package:expenses_tracker_flutter/widgets/expenses.dart';
 
 void main() {
   runApp(
