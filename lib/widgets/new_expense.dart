8947ceb9d65e4b02902d1fe63bb1fc00c4fc604a
Zazzaro <sassaro01cristian@gmail.com>
2024-05-07 18:26:15 +0200

    created new_expanse

---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class NewExpanse extends StatefulWidget {
+  const NewExpanse({super.key});
+  @override
+  State<StatefulWidget> createState() {
+    return _NewExpenseState();
+  }
+}
+
+class _NewExpenseState extends State<NewExpanse> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: const InputDecoration(
+              label: Text('title'),
+            ),
+          ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            maxLength: 10,
+            decoration: const InputDecoration(
+              prefixText: '\$',
+              label: Text('Amount'),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.text);
+                },
+                child: const Text(
+                  'Save Expense',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // todo add logic to exit editing and discard
+                },
+                child: const Text(
+                  'Discard',
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
