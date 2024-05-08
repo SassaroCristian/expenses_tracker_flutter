4d9a30901411926ce1ce947b179bcbcf0ca89af1
Zazzaro <sassaro01cristian@gmail.com>
2024-05-08 11:56:24 +0200

    Added snackbar to undo changes that lasts 3 sec

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -50,9 +50,23 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final indexExpense = _registerdExpenses.indexOf(expense);
     setState(() {
       _registerdExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted'),
+        action: SnackBarAction(
+            label: 'Undo',
+            onPressed: () {
+              setState(() {
+                _registerdExpenses.insert(indexExpense, expense);
+              });
+            }),
+      ),
+    );
   }
 
   @override
