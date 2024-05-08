e6eedcc412a7ee0e05d00ade59dca3eea9e4c8e5
Zazzaro <sassaro01cristian@gmail.com>
2024-05-08 11:49:26 +0200

    added documentation

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,3 +1,39 @@
+/*
+  *Documentation:
+
+  This file contains the Expense class and related constants for managing expenses.
+
+  The Expense class represents an expense entity with properties such as title, amount, date,
+  and category. It also provides methods for formatting date and generating unique IDs.
+
+  *Category: 
+  An enumeration representing different expense categories including food, travel,
+  leisure, and work.
+
+  categoryIcons: A map linking each expense category to its corresponding icon.
+
+  *Expense Class Properties:
+  - id: Unique identifier for the expense.
+  - title: Title of the expense.
+  - amount: Amount of the expense.
+  - date: Date of the expense.
+  - category: Category of the expense.
+
+  *Expense Class Methods:
+  - formattedDate: Method to format the date of the expense.
+
+  *Example usage:
+  - Creating an Expense object:
+    Expense(
+      title: 'Dinner',
+      amount: 50.0,
+      date: DateTime.now(),
+      category: Category.food,
+    )
+
+  !Note:
+*/
+
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 import 'package:intl/intl.dart';
