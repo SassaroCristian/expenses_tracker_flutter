de1a0f527b55223dc97a5ab4e64b018f734b2df0
Zazzaro <sassaro01cristian@gmail.com>
2024-05-08 09:38:47 +0200

    added date picker functionality

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
 
 class NewExpanse extends StatefulWidget {
   const NewExpanse({super.key});
@@ -11,6 +14,21 @@ class NewExpanse extends StatefulWidget {
 class _NewExpenseState extends State<NewExpanse> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
 
   @override
   void dispose() {
@@ -35,20 +53,50 @@ class _NewExpenseState extends State<NewExpanse> {
               label: Text('title'),
             ),
           ),
-          TextField(
-            controller: _amountController,
-            keyboardType: TextInputType.number,
-            maxLength: 10,
-            decoration: const InputDecoration(
-              prefixText: '\$',
-              label: Text('Amount'),
-            ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  maxLength: 10,
+                  decoration: const InputDecoration(
+                    prefixText: '\$',
+                    label: Text('Amount'),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      // ternary operator t chech for null and in case display text
+                      // ! <= is used to tell flutter a value wont be null
+                      _selectedDate == null
+                          ? 'No date selected'
+                          : formatter.format(_selectedDate!),
+                    ),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(
+                        Icons.calendar_month,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
           Row(
             children: [
               TextButton(
                 onPressed: () {
-                  // todo add logic to exit editing and discard
+                  Navigator.pop(context);
                 },
                 child: const Text(
                   'Discard',
