0b19bcd0b6d9109dc5b2d4fd9f5a9d322757324e
Zazzaro <sassaro01cristian@gmail.com>
2024-05-08 10:16:33 +0200

    changed a Map name

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -30,7 +30,7 @@ class ExpenseItem extends StatelessWidget {
                 const Spacer(),
                 Row(
                   children: [
-                    Icon(categoryItems[expense.category]),
+                    Icon(categoryIcons[expense.category]),
                     const SizedBox(
                       width: 8,
                     ),
